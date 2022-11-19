1e0d7dcc934e6598c00cd81cc4b355ccba8b2ecb
nmuru <47250209+nmuru@users.noreply.github.com>
2022-11-19 13:09:45 +0530

    Update setup_utils.py

--- tabpy/models/utils/setup_utils.py
+++ tabpy/models/utils/setup_utils.py
@@ -5,41 +5,41 @@ import sys
 from tabpy.tabpy_tools.client import Client
 
 
-# def get_default_config_file_path():
-#     import tabpy
+def get_default_config_file_path():
+    import tabpy
 
-#     pkg_path = os.path.dirname(tabpy.__file__)
-#     config_file_path = os.path.join(pkg_path, "tabpy_server", "common", "default.conf")
-#     return config_file_path
-
-
-# def parse_config(config_file_path):
-#     config = configparser.ConfigParser()
-#     config.read(config_file_path)
-#     tabpy_config = config["TabPy"]
-
-#     port = 9004
-#     if "TABPY_PORT" in tabpy_config:
-#         port = tabpy_config["TABPY_PORT"]
-
-#     auth_on = "TABPY_PWD_FILE" in tabpy_config
-#     ssl_on = (
-#         "TABPY_TRANSFER_PROTOCOL" in tabpy_config
-#         and "TABPY_CERTIFICATE_FILE" in tabpy_config
-#         and "TABPY_KEY_FILE" in tabpy_config
-#     )
-#     prefix = "https" if ssl_on else "http"
-#     return port, auth_on, prefix
+    pkg_path = os.path.dirname(tabpy.__file__)
+    config_file_path = os.path.join(pkg_path, "tabpy_server", "common", "default.conf")
+    return config_file_path
 
 
-# def get_creds():
-#     if sys.stdin.isatty():
-#         user = input("Username: ")
-#         passwd = getpass.getpass("Password: ")
-#     else:
-#         user = sys.stdin.readline().rstrip()
-#         passwd = sys.stdin.readline().rstrip()
-#     return [user, passwd]
+def parse_config(config_file_path):
+    config = configparser.ConfigParser()
+    config.read(config_file_path)
+    tabpy_config = config["TabPy"]
+
+    port = 9004
+    if "TABPY_PORT" in tabpy_config:
+        port = tabpy_config["TABPY_PORT"]
+
+    auth_on = "TABPY_PWD_FILE" in tabpy_config
+    ssl_on = (
+        "TABPY_TRANSFER_PROTOCOL" in tabpy_config
+        and "TABPY_CERTIFICATE_FILE" in tabpy_config
+        and "TABPY_KEY_FILE" in tabpy_config
+    )
+    prefix = "https" if ssl_on else "http"
+    return port, auth_on, prefix
+
+
+def get_creds():
+    if sys.stdin.isatty():
+        user = input("Username: ")
+        passwd = getpass.getpass("Password: ")
+    else:
+        user = sys.stdin.readline().rstrip()
+        passwd = sys.stdin.readline().rstrip()
+    return [user, passwd]
 
 
 def deploy_model(funcName, func, funcDescription):
