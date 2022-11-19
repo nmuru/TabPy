17f82837afa4460f5ae55a1f915bf83734f3554b
nmuru <47250209+nmuru@users.noreply.github.com>
2022-11-19 09:20:20 +0530

    Update predict_student.py

--- tabpy/models/scripts/predict_student.py
+++ tabpy/models/scripts/predict_student.py
@@ -2,9 +2,10 @@ import pickle
 from tabpy.tabpy_tools.client import Client
 import pandas as pd
 import numpy as np
+from tabpy.utils.model import setup_utils
 model=pickle.load(open('dtmodel.sav','rb'))
 
-tabpy
+# tabpy
 
 def predict_student(model,X_test):
     model=pickle.load(open('dtmodel.sav','rb'))
@@ -13,10 +14,14 @@ def predict_student(model,X_test):
 
  
 
-conn=Client('http://localhost:9004/')
+# conn=Client('http://localhost:9004/')
 
-conn.set_credentials('nmurugesh','N_Murugesh123')
+# conn.set_credentials('nmurugesh','N_Murugesh123')
 
-conn.deploy('predict_student',
-predict_student, 'Returns prediction of student conversion'
-, override = True)
+# conn.deploy('predict_student',
+# predict_student, 'Returns prediction of student conversion'
+# , override = True)
+
+
+if __name__ == "__main__":
+    setup_utils.deploy_model("predict_student", predict_student, "Returns the prediction")
